4d10896f8b2cc231d9ffb8afeaf33b40353c63f1
Chips <roslansaidi@ymail.com>
2017-03-31 00:39:12 +0800

    Fixed invite blade

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -1,7 +1,7 @@
-// make use of the named route so that if the URL ever changes, 
-// the form will not break #winning
+<!-- // make use of the named route so that if the URL ever changes, 
+// the form will not break #winning -->
 
-<form action="{{} route('invite') }}" method="POST">
+<form action="{{ route('invite') }}" method="POST">
 	{{ csrf_field() }}
 	<input type="email" name="email">
 	<button type="submit">Send Invite</button>
