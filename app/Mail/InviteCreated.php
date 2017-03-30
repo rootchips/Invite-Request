9c6ffc64a4fc818d655b7fe191908be74c660c35
Chips <roslansaidi@ymail.com>
2017-03-31 01:45:38 +0800

    Declare variable to public (We can use variable in blade view)

--- app/Mail/InviteCreated.php
+++ app/Mail/InviteCreated.php
@@ -6,18 +6,19 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use App\Invites;
+use App\Invite;
 
 class InviteCreated extends Mailable
 {
     use Queueable, SerializesModels;
-
+    
+    public $invite;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(Invites $invite)
+    public function __construct(Invite $invite)
     {
         $this->invite = $invite;
     }
