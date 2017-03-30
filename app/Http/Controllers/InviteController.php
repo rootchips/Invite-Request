3738d0861194ad9bcf386cbc6e6eb4ce41e47e27
Chips <roslansaidi@ymail.com>
2017-03-30 23:27:28 +0800

    Make controller & setup logic

---
+++ app/Http/Controllers/InviteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InviteController extends Controller
+{
+    public function invite()
+    {
+    	// user a form with an email field to invite a new user
+    }
+
+    public function process()
+    {
+    	// process the form submission and send the invite by email
+    }
+
+    public function accept($token)
+    {
+    	// here we'll look up the user by the token sent provided in the URL
+    }
+}
