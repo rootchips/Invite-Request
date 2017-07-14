3404a979065f5cc4ab36ad75f527f568e80236ab
Roslan Saidi <roslansaidi@ymail.com>
2017-07-14 03:58:57 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/InviteController.php
+++ app/Http/Controllers/InviteController.php
@@ -2,33 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
 use App\Invite;
 use App\Mail\InviteCreated;
+use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class InviteController extends Controller
 {
     public function invite()
     {
-    	// user a form with an email field to invite a new user
-    	return view('invite');
+        // user a form with an email field to invite a new user
+        return view('invite');
     }
 
     public function process(Request $request)
     {
-    	// process the form submission and send the invite by email
+        // process the form submission and send the invite by email
         do {
             // generate a random string using Laravel's str_random helper
             $token = str_random();
-        }
-        while (Invite::where('token', $token)->first());
+        } while (Invite::where('token', $token)->first());
 
         // create a new invite record
         $invite = Invite::create([
             'email' => $request->get('email'),
-            'token' => $token
+            'token' => $token,
             ]);
         // send the mail
         Mail::to($request->get('email'))->send(new InviteCreated($invite));
@@ -39,7 +38,7 @@ class InviteController extends Controller
 
     public function accept($token)
     {
-    	// here we'll look up the user by the token sent provided in the URL
+        // here we'll look up the user by the token sent provided in the URL
         // look up the invite
         if (!$invite = Invite::where('token', $token)->first()) {
             // if the invite doesn't exit do something more graceful than this
--- app/Invite.php
+++ app/Invite.php
@@ -4,9 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Invite extends Model 
+class Invite extends Model
 {
-	protected $fillable = [
-	'email', 'token',
-	];
+    protected $fillable = [
+    'email', 'token',
+    ];
 }
